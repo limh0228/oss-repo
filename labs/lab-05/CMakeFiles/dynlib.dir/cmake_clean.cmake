3029a45e3052e56518a2a5db88d41a54e2146d62
Hannah Lim <lim.hannah0228@gmail.com>
2019-06-27 19:28:18 -0400

    lab 5 part 2

---
+++ labs/lab-05/CMakeFiles/dynlib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dynlib.dir/source/block.c.o"
+  "libdynlib.pdb"
+  "libdynlib.dylib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dynlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ labs/lab-05/CMakeFiles/staticlib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/staticlib.dir/source/block.c.o"
+  "libstaticlib.pdb"
+  "libstaticlib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/staticlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
